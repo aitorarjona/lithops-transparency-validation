8a09f88359a8883a70bf32d161ff3a22fd1f7584
Aitor <aitor.a98@gmail.com>
2021-11-18 19:02:05 +0100

    replaced cprofile with time.time

--- micro-benchmarks/experiments/profiling/func.py
+++ micro-benchmarks/experiments/profiling/func.py
@@ -1,6 +1,7 @@
 import hashlib
 import cProfile, pstats
 import concurrent.futures
+import time
 from config import *
 
 if LITHOPS:
@@ -102,8 +103,8 @@ def allreduce_master(worker_conns):
 
 
 def allreduce_worker(proc_id, mast_conn):
-    profiler = cProfile.Profile()
-    profiler.enable()
+    compute_time = 0.0
+    comm_time = 0.0
 
     data = bytes(BYTE_SIZE)
     payload = bytes(PAYLOAD_SIZE)
@@ -111,24 +112,20 @@ def allreduce_worker(proc_id, mast_conn):
         print(f"[{proc_id}] round {i}")
 
         hash_ = hashlib.sha512()
+        t0 = time.time()
         for _ in range(HASH_LOOP_COUNT):
             hash_.update(data)
+        t1 = time.time()
+        td = t1 - t0
+        compute_time += td
         
+        t0 = time.time()
         mast_conn.send(payload)
         _ = mast_conn.recv()
+        t1 = time.time()
+        td = t1 - t0
+        comm_time += td
 
         print(f"[{proc_id}]", hash_.hexdigest())
 
-    profiler.disable()
-    stats = pstats.Stats(profiler).sort_stats("cumtime")
-    stats.dump_stats(f"/tmp/stats_{proc_id}.prof")
-
-    if LITHOPS:
-        import lithops.storage as storage
-
-        sto = storage.Storage()
-        with open(f"/tmp/stats_{proc_id}.prof", "rb") as f:
-            stat_data = f.read()
-            sto.put_object(
-                bucket="aitor-data", key=f"tmp/stats_{proc_id}.prof", body=stat_data
-            )
+    return {'compute_time': compute_time, 'comm_time': comm_time}
