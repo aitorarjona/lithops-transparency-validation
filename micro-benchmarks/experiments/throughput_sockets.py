bc57fc283a52ca61dcfd983f8ba5056abb2b2e42
Aitor <aitor.a98@gmail.com>
2021-11-23 12:10:34 +0100

    fix sockets

--- micro-benchmarks/experiments/throughput_sockets.py
+++ micro-benchmarks/experiments/throughput_sockets.py
@@ -1,45 +1,57 @@
-import numpy as np
 import time
-import random
-import math
+import struct
 import argparse
-import logging
 import time
-import concurrent.futures
 import socket
-import io
 
 
 def client(server, server_port, payload_sz):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((server, server_port))
+    length = struct.pack('>Q', payload_sz)
+    s.sendall(length)
     data = bytes(payload_sz)
     t0 = time.time()
-    s.send(data)
+    s.sendall(data)
     t1 = time.time()
+    s.shutdown(socket.SHUT_WR)
     s.close()
-    print(t1 - t0)
+
+    td = t1 - t0
+    print(td)
+    throughput = (payload_sz / td) / 1_000_000
+    print('Throughput = {} MB/s'.format(throughput))
 
 
-def server(bind_addr, sever_port, payload):
+def server(bind_addr, sever_port):
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print(f'listening on {bind_addr}:{sever_port}')
     serversocket.bind((bind_addr, sever_port))
-    serversocket.listen()
+    serversocket.listen(1)
 
     (clientsocket, address) = serversocket.accept()
-    t0 = time.time()
-    data = clientsocket.recv(payload)
-    t1 = time.time()
+    try:
+        bs = clientsocket.recv(8)
+        (length,) = struct.unpack('>Q', bs)
+        print(length)
+        data = b''
+        t0 = time.time()
+        while len(data) < length:
+            to_read = length - len(data)
+            print(to_read)
+            data += clientsocket.recv(length if to_read > length else to_read)
+    finally:
+        t1 = time.time()
+        clientsocket.shutdown(socket.SHUT_WR)
+        clientsocket.close()
     print(len(data))
-    clientsocket.close()
+    
     td = t1 - t0
     print(td)
-
-    throughput = (payload / td) / 1_000_000
+    throughput = (length / td) / 1_000_000
     print('Throughput = {} MB/s'.format(throughput))
-    
-    serversocket.shutdown(socket.SHUT_RD)
+
+    serversocket.close()
 
 
 if __name__ == '__main__':
@@ -51,7 +63,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.kind == 'server':
-        server(args.host, args.port, args.payload)
+        server(args.host, args.port)
     elif args.kind == 'client':
         client(args.host, args.port, args.payload)
     else:
